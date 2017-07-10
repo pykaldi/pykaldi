8593e16bd7e128fb8ff2a1b1bcb5f25beed6fa6b
Victor Martinez <vrmp00@gmail.com>
2017-07-10 01:39:26 -0700

    Added matrix functions to __init__.py. I havent tried them yet

--- kaldi/matrix/__init__.py
+++ kaldi/matrix/__init__.py
@@ -147,11 +147,138 @@ class SubVector(kaldi_vector.SubVector, _VectorBase):
         else:
             raise TypeError("SubVector index must be an integer or a slice.")
 
+class _MatrixBase(object):
+    def shape(self):
+        """Returns dimensions of matrix"""
+        return self.num_rows_, self.num_cols_
 
-class Matrix(kaldi_matrix.Matrix):
+    def nrows(self):
+        """Returns number of rows"""
+        return self.num_rows_
+
+    def ncols(self):
+        """Returns number of columns"""
+        return self.num_cols_
+
+    def range(self, row_offset, row_length, col_offset, col_length):
+        """Returns a new submatrix of the matrix."""
+        return SubMatrix(row_offset, row_length, col_offset, col_length)
+
+class SubMatrix(kaldi_matrix.SubMatrix, _MatrixBase):
+    def __init__(self, src, row_offset = 0, row_length = None, 
+                            col_offset = 0, col_length = None):
+        """Creates a new submatrix from the source (sub)matrix
+           If 'length' is None, it defaults to rows (or cols) - offset.
+
+           Args:
+                src (MatrixBase): Source (sub)matrix.
+                row_offset, col_offset: Start of the submatrix.
+                row_length, col_length: Length of the submatrix.
+        """
+        src_rows, src_cols = src.shape()
+        if not (0 <= row_offset < src_rows):
+            raise IndexError("Row offset={} should be in the range "
+                             "[0,{}) when nrows(src)={}."
+                             .format(row_offset, src_rows, src_rows))
+        if not (0 <= col_offset < src_cols):
+            raise IndexError("Col offset={} should be in the range "
+                             "[0,{}) when ncols(src)={}."
+                             .format(col_offset, src_cols, src_cols))
+
+        max_row_len, max_col_len = src_rows - row_offset, src_cols - col_offset
+        if row_length is None:
+            row_length = max_row_len
+        if col_length is None:
+            col_length = max_col_len
+
+        if not (0 <= row_length <= max_row_len):
+            raise IndexError("Row length={} should be in the range "
+                             "[0,{}) when offset={} and nrows(src)={}."
+                             .format(row_length, max_row_len, row_offset, src_rows))
+        if not (0 <= col_length <= max_col_len):
+            raise IndexError("Col length={} should be in the range "
+                             "[0,{}) when offset={} and ncols(src)={}."
+                             .format(col_length, max_col_len, col_offset, src_cols))
+
+        super(SubMatrix, self).__init__(src, row_offset, row_length,
+                                             col_offset, col_length)
+
+
+class Matrix(kaldi_matrix.Matrix, _MatrixBase):
     """Python wrapper for kaldi::Matrix<float>"""
-    def __init__(self):
+    def __init__(self, size=None, **kwargs):
+        """Creates a new Matrix.
+            
+        If no arguments are given, returns a new empty matrix.
+        'size' is a tuple (or list) of matrix dimensions. If given, 
+                return matrix with dimension size[0] x size[1].
+
+        Args:
+            size (tuple or list): Matrix dimensions
+        """
         super(Matrix, self).__init__()
+        
+        if size is not None:
+            if not (isinstance(size, tuple) or isinstance(size, list)):
+                raise TypeError("Matrix size argument must be a tuple or list.")
+
+            if len(size) != 2:
+                raise TypeError("Matrix size argument must be of length 2.")
+
+            if not (isinstance(size[0], int) and isinstance(size[1], int)):
+                raise TypeError("Matrix dimensions must be integer.")
+
+            if not (size[0] > 0 and size[1] > 0):
+                raise TypeError("Matrix dimensions must be positive.")
+
+            # TODO (VM):
+            # Include missing parameters here
+            self.resize_(size[0], size[1])
+
+
+    # TODO (VM):
+    # Test this out
+    def __getitem__(self, index):
+        """Custom indexing method
+
+        Returns:
+            - the value at the given index if the index is an integer tuple 
+            - a submatrix if at least one index is a slice with step = 1
+        """
+
+        if isinstance(index, tuple):
+            if len(index) != 2:
+                raise IndexError("too many indices for Matrix")  
+
+            # Simple indexing by two integers
+            if isinstance(index[0], int) and isinstance(index[1], int):
+                return self.getitem_(index[0], index[1])
+
+            # 
+            row_offset, col_offset = 0, 0
+            row_end, col_end = self.shape()
+            row_step, col_step = 1, 1
+
+            if isinstance(index[0], int):
+                row_offset = index[0]
+            elif isinstance(index[0], slice):
+                row_offset, row_end, row_step = index[0].indices(self.num_rows_)
+                if row_step != 1:
+                    raise NotImplementedError("slicing with steps not implemented yet")
+
+            if isinstance(index[1], int):
+                col_offset = index[1]
+            elif isinstance(index[1], slice):
+                col_offset, col_end, col_step = index[0].indices(self.num_cols_)
+                if col_step != 1:
+                    raise NotImplementedError("slicing with steps not implemented yet")
+
+            return self.range(row_offset, row_end - row_offset, 
+                              col_offset, col_end - col_offset)
+
+            
+        raise IndexError("something went wrong")  
+
 
 ################################################################################
 # Define Vector and Matrix Utility Functions
