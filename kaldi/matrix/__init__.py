e76325253b27d15e8f4a085a916a9c4805aa5cf3
Victor Martinez <vrmp00@gmail.com>
2017-07-11 13:32:21 -0700

    Small fixes to tmLanguage (subl only). Fixed matrix subscripting

--- kaldi/matrix/__init__.py
+++ kaldi/matrix/__init__.py
@@ -46,9 +46,11 @@ class _VectorBase(object):
         self.CopyFromVec(src)
         return self
 
+    # Note (VM):
+    # Was missing a return
     def equal(self, other, tol=0.0):
         """True if two vectors have the same size and data, false otherwise."""
-        self.ApproxEqual(other, tol)
+        return self.ApproxEqual(other, tol)
 
     def numpy(self):
         """Returns this vector as a numpy ndarray."""
@@ -62,6 +64,8 @@ class _VectorBase(object):
 class Vector(kaldi_vector.Vector, _VectorBase):
     """Python wrapper for kaldi::Vector<float>"""
 
+    # Note (VM):
+    # Missing parameter check (e.g., v = Vector(-1) crashes hard)
     def __init__(self, size=None, src=None):
         """Creates a new vector.
 
@@ -162,10 +166,98 @@ class _MatrixBase(object):
         """Returns number of columns"""
         return self.num_cols_
 
+    def equal(self, other, tol = 1e-16):
+        """True if two matrices have the same size and data within tol. False otherwise"""
+        return self.ApproxEqual(other, tol)
+
     def range(self, row_offset, row_length, col_offset, col_length):
         """Returns a new submatrix of the matrix."""
         return SubMatrix(self, row_offset, row_length, col_offset, col_length)
 
+    def __getitem__(self, index):
+        """Custom indexing method
+
+        Returns:
+            - the value at the given index if the index is an integer tuple
+            - a submatrix if at least one index is a slice with step = 1
+            - a list if at least one index is a slice with step > 1
+        """
+        if isinstance(index, tuple):
+            if len(index) != 2:
+                raise IndexError("too many indices for {}".format(self.__class__.__name__))  
+
+            # Simple indexing by two integers
+            if isinstance(index[0], int) and isinstance(index[1], int):
+                if index[0] >= self.nrows() or index[1] >= self.ncols():
+                    raise IndexError("indices out of bounds.")
+
+                if index[0] < 0 or index[1] < 0:
+                    raise NotImplementedError("negative indices not implemented.")
+
+                return self._getitem(index[0], index[1]) #Call C-impl
+
+            # Indexing by two slices
+            if isinstance(index[0], slice) and isinstance(index[1], slice):
+                row_start, row_end, row_step = index[0].indices(self.nrows())
+                col_start, col_end, col_step = index[0].indices(self.ncols())
+
+                if row_step == 1 and col_step == 1:
+                    return self.range(row_start, row_end - row_start, col_start, col_end - col_start)
+                elif row_step == 1:
+                    return [self.__getitem__((index[0], j)) for j in xrange(col_start, col_end, col_step)]
+                elif col_step == 1:
+                    return [self.__getitem__((i, index[1])) for i in xrange(row_start, row_end, row_step)]
+
+            # Row is a slice, Col is an int
+            if isinstance(index[0], slice):
+                start, end, step = index[0].indices(self.nrows())
+                if step == 1:
+                    return self.range(start, end - start, index[1], 1)
+                else:
+                    return [self.__getitem__((i, index[1])) for i in xrange(start, end, step)]
+
+            # Row is an int, Col is a slice
+            if isinstance(index[1], slice):
+                start, end, step = index[1].indices(self.ncols())
+                if step == 1:
+                    return self.range(index[0], 1, start, end - start)
+                else:
+                    return [self.__getitem__((index[0], j)) for j in xrange(start, end, step)]
+
+
+        raise IndexError("{} index must be a tuple".format(self.__class__.__name__))
+
+    
+class Matrix(kaldi_matrix.Matrix, _MatrixBase):
+    """Python wrapper for kaldi::Matrix<float>"""
+    def __init__(self, size=None, **kwargs):
+        """Creates a new Matrix.
+
+        If no arguments are given, returns a new empty matrix.
+        'size' is a tuple (or list) of matrix dimensions. If given,
+                return matrix with dimension size[0] x size[1].
+
+        Args:
+            size (tuple or list): Matrix dimensions
+        """
+        super(Matrix, self).__init__()
+
+        if size is not None:
+            if not (isinstance(size, tuple) or isinstance(size, list)):
+                raise TypeError("Matrix size argument must be a tuple or list.")
+
+            if len(size) != 2:
+                raise TypeError("Matrix size argument must be of length 2.")
+
+            if not (isinstance(size[0], int) and isinstance(size[1], int)):
+                raise TypeError("Matrix dimensions must be integer.")
+
+            if not (size[0] > 0 and size[1] > 0):
+                if not (size[0] == 0 and size[1] == 0):
+                    raise TypeError("Both Matrix dimensions must be positive "
+                                    "or both of them should be 0.")
+
+            self.resize_(size[0], size[1], MatrixResizeType.UNDEFINED)
 
 class SubMatrix(kaldi_matrix.SubMatrix, _MatrixBase):
     def __init__(self, src, row_offset = 0, rows = None,
@@ -207,85 +299,6 @@ class SubMatrix(kaldi_matrix.SubMatrix, _MatrixBase):
 
         super(SubMatrix, self).__init__(src, row_offset, rows, col_offset, cols)
 
-
-class Matrix(kaldi_matrix.Matrix, _MatrixBase):
-    """Python wrapper for kaldi::Matrix<float>"""
-    def __init__(self, size=None, **kwargs):
-        """Creates a new Matrix.
-
-        If no arguments are given, returns a new empty matrix.
-        'size' is a tuple (or list) of matrix dimensions. If given,
-                return matrix with dimension size[0] x size[1].
-
-        Args:
-            size (tuple or list): Matrix dimensions
-        """
-        super(Matrix, self).__init__()
-
-        if size is not None:
-            if not (isinstance(size, tuple) or isinstance(size, list)):
-                raise TypeError("Matrix size argument must be a tuple or list.")
-
-            if len(size) != 2:
-                raise TypeError("Matrix size argument must be of length 2.")
-
-            if not (isinstance(size[0], int) and isinstance(size[1], int)):
-                raise TypeError("Matrix dimensions must be integer.")
-
-            if not (size[0] > 0 and size[1] > 0):
-                if not (size[0] == 0 and size[1] == 0):
-                    raise TypeError("Both Matrix dimensions must be positive "
-                                    "or both of them should be 0.")
-
-            # TODO (VM):
-            # Include missing parameters here
-            self.resize_(size[0], size[1])
-
-
-    # TODO (VM):
-    # Test this out
-    def __getitem__(self, index):
-        """Custom indexing method
-
-        Returns:
-            - the value at the given index if the index is an integer tuple
-            - a submatrix if at least one index is a slice with step = 1
-        """
-
-        if isinstance(index, tuple):
-            if len(index) != 2:
-                raise IndexError("too many indices for Matrix")
-
-            # Simple indexing by two integers
-            if isinstance(index[0], int) and isinstance(index[1], int):
-                return self.getitem_(index[0], index[1])
-
-            #
-            row_offset, col_offset = 0, 0
-            row_end, col_end = self.shape()
-            row_step, col_step = 1, 1
-
-            if isinstance(index[0], int):
-                row_offset = index[0]
-            elif isinstance(index[0], slice):
-                row_offset, row_end, row_step = index[0].indices(self.num_rows_)
-                if row_step != 1:
-                    raise NotImplementedError("slicing with steps not implemented yet")
-
-            if isinstance(index[1], int):
-                col_offset = index[1]
-            elif isinstance(index[1], slice):
-                col_offset, col_end, col_step = index[0].indices(self.num_cols_)
-                if col_step != 1:
-                    raise NotImplementedError("slicing with steps not implemented yet")
-
-            return self.range(row_offset, row_end - row_offset,
-                              col_offset, col_end - col_offset)
-
-
-        raise IndexError("something went wrong")
-
-
 ################################################################################
 # Define Vector and Matrix Utility Functions
 ################################################################################
@@ -296,12 +309,4 @@ def vector_to_numpy(vector):
 
 def numpy_to_vector(array):
     """Converts a numpy array to a SubVector."""
-    return SubVector(kaldi_numpy.numpy_to_vector(array))
-
-def matrix_to_numpy(matrix):
-    """Converts a Matrix to a numpy array."""
-    return kaldi_numpy.matrix_to_numpy(matrix)
-
-def numpy_to_matrix(array):
-    """Converts a numpy array to a SubMatrix."""
-    return SubMatrix(kaldi_numpy.numpy_to_matrix(array))
+    return SubVector(kaldi_vector_numpy.numpy_to_vector(array))
