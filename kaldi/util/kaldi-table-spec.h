58d00e8087439399a6a4b29dfd025504d6a14594
Victor Martinez <vrmp00@gmail.com>
2017-07-20 01:24:05 -0700

    it builds

--- kaldi/util/kaldi-table-spec.h
+++ kaldi/util/kaldi-table-spec.h
@@ -4,14 +4,12 @@
 namespace kaldi {
 
     template<typename Real>
-    class SequentialKaldiVectorTableReader : public SequentialTableReaderImplBase<KaldiObjectHolder<Vector<Real>>>{
+    class SequentialKaldiVectorTableReader : public SequentialTableReader<KaldiObjectHolder<Vector<Real>>>{
     public:
-        SequentialKaldiVectorTableReader() : SequentialTableReaderImplBase<KaldiObjectHolder<Vector<Real>>>() { }
-        SequentialKaldiVectorTableReader(const std::string &rspecifier) : SequentialTableReaderImplBase<KaldiObjectHolder<Vector<Real>>>(&rspecifier) { }
-        const Vector<Real> &Value(){
-            SequentialTableReaderImplBase<KaldiObjectHolder<Vector<Real>>>::CheckImpl();
-            return SequentialTableReaderImplBase<KaldiObjectHolder<Vector<Real>>>::impl_->Value();  // This may throw (if EnsureObjectLoaded() returned false you
-                                    // are safe.).
+        SequentialKaldiVectorTableReader() : SequentialTableReader<KaldiObjectHolder<Vector<Real>>>() { }
+        SequentialKaldiVectorTableReader(const std::string &rspecifier) : SequentialTableReader<KaldiObjectHolder<Vector<Real>>>(&rspecifier) { }
+        const Vector<Real> &Value() {
+            return SequentialTableReader<KaldiObjectHolder<Vector<Real>>>::Value();
         }
     };
 
