7c4d7a3230c4447a2b1852a4b7044da25dc1b133
Victor Martinez <vrmp00@gmail.com>
2017-09-24 11:30:00 -0700

    Standarizing API: nnet3; small fixes to cudamatrix

--- kaldi/cudamatrix/__init__.py
+++ kaldi/cudamatrix/__init__.py
@@ -7,3 +7,7 @@ from . import matrix
 __all__ = [name for name in dir()
            if name[0] != '_'
            and not name.endswith('Base')]
+
+# Expose cuda_available as a part of this model,
+# but leave it out of the __all__
+from .device import cuda_available
--- tests/nnet3/nnet-compute-test.py
+++ tests/nnet3/nnet-compute-test.py
@@ -5,7 +5,8 @@ import unittest
 
 from kaldi.base.io import stringstream as sstream
 from kaldi.base.io import istringstream, ostringstream
-from kaldi.cudamatrix.matrix import ApproxEqualCuMatrix, CuMatrix
+from kaldi.cudamatrix.matrix import approx_equal_cu_matrix, CuMatrix
+from kaldi.cudamatrix.device import cuda_available
 from kaldi.matrix import Matrix, Vector
 from kaldi.matrix.vector import ApproxEqualVector
 from kaldi.nnet3 import *
@@ -16,64 +17,64 @@ class TestNnetCompute(unittest.TestCase):
         gen_config = NnetGenerationOptions()
         test_collapse_model = random.choice([True, False])
 
-        configs = GenerateConfigSequence(gen_config)
+        configs = generate_config_sequence(gen_config)
         nnet = Nnet()
         for j, config in enumerate(configs):
             print("Input config[{}]:".format(j))
             print(config)
             istrm = istringstream.new_from_str(config)
-            nnet.ReadConfig(istrm)
+            nnet.read_config(istrm)
 
         request = ComputationRequest()
-        inputs = ComputeExampleComputationRequestSimple(nnet, request)
+        inputs = compute_example_computation_request_simple(nnet, request)
         if test_collapse_model:
-            SetBatchnormTestMode(True, nnet)
-            SetDropoutTestMode(True, nnet)
+            set_batchnorm_test_mode(True, nnet)
+            set_dropout_test_mode(True, nnet)
 
         compiler = Compiler(request, nnet)
         opts = CompilerOptions()
-        computation = compiler.CreateComputation(opts)
+        computation = compiler.create_computation(opts)
 
         nnet_collapsed = Nnet.new_from_other(nnet)
         if test_collapse_model:
             collapse_config = CollapseModelConfig()
-            CollapseModel(collapse_config, nnet_collapsed)
+            collapse_model(collapse_config, nnet_collapsed)
             compiler_collapsed = Compiler(request, nnet_collapsed)
-            computation_collapsed = compiler_collapsed.CreateComputation(opts)
-            computation_collapsed.ComputeCudaIndexes()
+            computation_collapsed = compiler_collapsed.create_computation(opts)
+            computation_collapsed.compute_cuda_indexes()
 
         ostrm = ostringstream()
-        computation.Print(ostrm, nnet)
+        computation.print(ostrm, nnet)
         print("Generated computation:")
         print(ostrm.to_str())
 
         check_config = CheckComputationOptions()
         check_config.check_rewrite = True
         checker = ComputationChecker(check_config, nnet, computation)
-        checker.Check()
+        checker.check()
 
         if random.choice([True, False]):
             opt_config = NnetOptimizeOptions()
-            Optimize(opt_config, nnet, MaxOutputTimeInRequest(request),
+            optimize(opt_config, nnet, max_output_time_in_request(request),
                      computation)
             ostrm = ostringstream()
-            computation.Print(ostrm, nnet)
+            computation.print(ostrm, nnet)
             print("Optimized computation:")
             print(ostrm.to_str())
 
         compute_opts = NnetComputeOptions()
         compute_opts.debug = random.choice([True, False])
-        computation.ComputeCudaIndexes()
+        computation.compute_cuda_indexes()
         computer = NnetComputer(compute_opts, computation, nnet, nnet)
 
         for i, ispec in enumerate(request.inputs):
             temp = CuMatrix.new_from_matrix(inputs[i])
-            print("Input sum:", temp.Sum())
-            computer.AcceptInput(ispec.name, temp)
-        computer.Run()
+            print("Input sum:", temp.sum())
+            computer.accept_input(ispec.name, temp)
+        computer.run()
 
-        output = computer.GetOutputDestructive("output")
-        print("Output sum:", output.Sum())
+        output = computer.get_output_destructive("output")
+        print("Output sum:", output.sum())
 
         if test_collapse_model:
             computer_collapsed = NnetComputer(compute_opts,
@@ -81,44 +82,44 @@ class TestNnetCompute(unittest.TestCase):
                                               nnet_collapsed, nnet_collapsed)
             for i, ispec in enumerate(request.inputs):
                 temp = CuMatrix.new_from_matrix(inputs[i])
-                print("Input sum:", temp.Sum())
-                computer_collapsed.AcceptInput(ispec.name, temp)
-            computer_collapsed.Run()
-            output_collapsed = computer_collapsed.GetOutputDestructive("output")
-            print("Output sum [collapsed]:", output_collapsed.Sum())
-            self.assertTrue(ApproxEqualCuMatrix(output, output_collapsed),
+                print("Input sum:", temp.sum())
+                computer_collapsed.accept_input(ispec.name, temp)
+            computer_collapsed.run()
+            output_collapsed = computer_collapsed.get_output_destructive("output")
+            print("Output sum [collapsed]:", output_collapsed.sum())
+            self.assertTrue(approx_equal_cu_matrix(output, output_collapsed),
                             "Regular and collapsed computation outputs differ.")
 
-        output_deriv = CuMatrix.new_from_size(output.NumRows(),
-                                              output.NumCols())
-        output_deriv.SetRandn()
+        output_deriv = CuMatrix.new_from_size(output.num_rows(),
+                                              output.num_cols())
+        output_deriv.set_randn()
         if request.outputs[0].has_deriv:
-            computer.AcceptInput("output", output_deriv)
-            computer.Run()
+            computer.accept_input("output", output_deriv)
+            computer.run()
             for i, ispec in enumerate(request.inputs):
                 if ispec.has_deriv:
-                    in_deriv = computer.GetOutputDestructive(ispec.name)
+                    in_deriv = computer.get_output_destructive(ispec.name)
                     print("Input-deriv sum for input {} is:".format(ispec.name),
-                          in_deriv.Sum())
+                          in_deriv.sum())
 
     def test_nnet_decodable(self):
         gen_config = NnetGenerationOptions()
-        configs = GenerateConfigSequence(gen_config)
+        configs = generate_config_sequence(gen_config)
         nnet = Nnet()
         for j, config in enumerate(configs):
             print("Input config[{}]:".format(j))
             print(config)
             istrm = istringstream.new_from_str(config)
-            nnet.ReadConfig(istrm)
+            nnet.read_config(istrm)
 
         num_frames = 5 + random.randint(1, 100)
-        input_dim = nnet.InputDim("input")
-        output_dim = nnet.OutputDim("output")
-        ivector_dim = max(0, nnet.InputDim("ivector"))
+        input_dim = nnet.input_dim("input")
+        output_dim = nnet.output_dim("output")
+        ivector_dim = max(0, nnet.input_dim("ivector"))
         input = Matrix(num_frames, input_dim)
 
-        SetBatchnormTestMode(True, nnet)
-        SetDropoutTestMode(True, nnet)
+        set_batchnorm_test_mode(True, nnet)
+        set_dropout_test_mode(True, nnet)
 
         input.set_randn()
         ivector = Vector(ivector_dim)
@@ -138,18 +139,18 @@ class TestNnetCompute(unittest.TestCase):
         decodable = DecodableNnetSimple(opts, nnet, priors, input, compiler,
                                         ivector if ivector_dim else None)
         for t in range(num_frames):
-            decodable.GetOutputForFrame(t, output1[t])
+            decodable.get_output_for_frame(t, output1[t])
 
         opts = NnetSimpleLoopedComputationOptions()
         info = DecodableNnetSimpleLoopedInfo.new_from_priors(opts, priors, nnet)
         decodable = DecodableNnetSimpleLooped(info, input,
                                               ivector if ivector_dim else None)
         for t in range(num_frames):
-            decodable.GetOutputForFrame(t, output2[t])
+            decodable.get_output_for_frame(t, output2[t])
 
-        if (not NnetIsRecurrent(nnet)
-            and nnet.Info().find("statistics-extraction") == -1
-            and nnet.Info().find("TimeHeightConvolutionComponent") == -1):
+        if (not nnet_is_recurrent(nnet)
+            and nnet.info().find("statistics-extraction") == -1
+            and nnet.info().find("TimeHeightConvolutionComponent") == -1):
             for t in range(num_frames):
                 self.assertTrue(ApproxEqualVector(output1[t], output2[t]))
 
@@ -157,10 +158,10 @@ class TestNnetCompute(unittest.TestCase):
 if __name__ == '__main__':
     for i in range(2):
         if cuda_available():
-            from kaldi.cudamatrix import CuDevice
-            CuDevice.Instantiate().SetDebugStrideMode(True)
+            from kaldi.cudamatrix.device import CuDevice
+            CuDevice.instantiate().set_debug_stride_mode(True)
             if i == 0:
-                CuDevice.Instantiate().SelectGpuId("no")
+                CuDevice.instantiate().select_gpu_id("no")
             else:
-                CuDevice.Instantiate().SelectGpuId("yes")
+                CuDevice.instantiate().select_gpu_id("yes")
         unittest.main(exit=False)
