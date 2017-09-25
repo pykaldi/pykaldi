fea10b04af561b4e77e7d069e161e743a6522336
Victor Martinez <vrmp00@gmail.com>
2017-09-24 21:58:35 -0700

    Added python wrappers forHMMs

--- kaldi/hmm/transition_model.py
+++ kaldi/hmm/transition_model.py
@@ -1,5 +1,25 @@
-from ._transition_model import *
+from . import _transition_model
 from ._tree_accu import *
+
+from _transition_model import get_pdfs_for_phones, get_phones_for_pdfs
+
+class TransitionModel(_transition_model.TransitionModel):
+	"""Wrapper for kaldi TransitionModel.
+
+	Provides a more Pythonic access for the API facing the user.
+	"""
+	def __init__(self):
+		super(TransitionModel, self).__init__()
+
+	def print(self, os, phone_names, occs = None):
+		"""Safe-version of Kaldi's TransitionModel.print
+        Checks for empty _phones."""
+		if len(self.get_phones()) == 0:
+			raise Exception("TransitionModel empty phone list")
+		if occs is not None:
+			self._print(os, phone_names, occs)
+		else:
+			self._print(os, phone_names)
 
 ################################################################################
 
---
+++ tests/hmm/hmm-topology-test.py
@@ -0,0 +1,127 @@
+from collections import defaultdict
+
+from kaldi.base.math import *
+from kaldi.hmm.topology import *
+from kaldi.base.io import istringstream, ostringstream
+
+import unittest
+
+def get_default_topology(phones):
+    phones = sorted(phones)
+
+    topo_string = """<Topology>
+    <TopologyEntry>
+    <ForPhones> 
+    """
+    for i in phones:
+        topo_string += str(i) + " "
+
+    topo_string += """</ForPhones>
+    <State> 0 <PdfClass> 0
+    <Transition> 0 0.5
+    <Transition> 1 0.5
+    </State>
+    <State> 1 <PdfClass> 1 
+    <Transition> 1 0.5
+    <Transition> 2 0.5
+    </State>
+    <State> 2 <PdfClass> 2
+    <Transition> 2 0.5
+    <Transition> 3 0.5
+    </State>
+    <State> 3 </State>
+    </TopologyEntry>
+    </Topology>
+    """
+
+    topo_string = ostringstream.new_from_str(topo_string)
+
+    topo = HmmTopology()
+    iss = istringstream.new_from_str(topo_string.to_str())
+    topo.read(iss, False)
+    return topo
+
+class TestHMMTopology(unittest.TestCase):
+
+    def testHmmTopology(self):
+
+        input_str = """<Topology>
+        <TopologyEntry>
+        <ForPhones> 1 2 3 4 5 6 7 8 9 </ForPhones>
+        <State> 0 <PdfClass> 0 
+        <Transition> 0 0.5
+        <Transition> 1 0.5
+        </State>
+        <State> 1 <PdfClass> 1
+        <Transition> 1 0.5
+        <Transition> 2 0.5
+        </State>
+        <State> 2 <PdfClass> 2
+        <Transition> 2 0.5
+        <Transition> 3 0.5
+        </State>
+        <State> 3 </State>
+        </TopologyEntry>
+        <TopologyEntry>
+        <ForPhones> 10 11 13 </ForPhones>
+        <State> 0 <PdfClass> 0
+        <Transition> 0 0.5
+        <Transition> 1 0.5
+        </State>
+        <State> 1 <PdfClass> 1
+        <Transition> 1 0.5
+        <Transition> 2 0.5
+        </State>
+        <State> 2 </State>
+        </TopologyEntry>
+        </Topology>"""
+
+        chain_input_str = """<Topology>
+        <TopologyEntry>
+        <ForPhones> 1 2 3 4 5 6 7 8 9 </ForPhones>
+        <State> 0 <ForwardPdfClass> 0 <SelfLoopPdfClass> 1
+        <Transition> 0 0.5
+        <Transition> 1 0.5
+        </State>
+        <State> 1 </State>
+        </TopologyEntry>
+        </Topology>
+        """
+
+        for i in range(10):
+            binary = True if (rand()%2 == 0) else False
+
+            with self.subTest(binary = binary):
+                topo = HmmTopology()
+
+                iss = istringstream.new_from_str(input_str)
+                topo.read(iss, False)
+                self.assertEqual(3, topo.min_length(3))
+                self.assertEqual(2, topo.min_length(11))
+
+                oss = ostringstream()
+                topo.write(oss, binary)
+
+                topo2 = HmmTopology()
+                iss2 = istringstream.new_from_str(oss.to_str())
+                topo2.read(iss2, binary)
+
+                # Test equality
+                oss1 = ostringstream()
+                oss2 = ostringstream()
+                topo.write(oss1, False)
+                topo2.write(oss2, False)
+                self.assertEqual(oss1.to_str(), oss2.to_str())
+
+                # Test chain topology
+                chain_topo = HmmTopology()
+                chain_iss = istringstream.new_from_str(chain_input_str)
+                chain_topo.read(chain_iss, False)
+                self.assertEqual(1, chain_topo.min_length(3))
+
+                # make sure get_default_topology doesnt crash
+                phones = [1, 2]
+                get_default_topology(phones)
+
+if __name__ == '__main__':
+    unittest.main()
