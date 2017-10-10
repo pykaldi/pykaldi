7c07ce6d42f2a96822fbed59774e8f50136d2bbb
Victor Martinez <vrmp00@gmail.com>
2017-10-10 00:48:48 -0700

    Addresses https://github.com/usc-sail/pykaldi/commit/eba15be38b69509eed593a2ac99fbd69915476cc#commitcomment-24845303

--- kaldi/online2/__init__.py
+++ kaldi/online2/__init__.py
@@ -1,6 +1,7 @@
 from ._online_timing import *
 from ._online_endpoint import *
 from ._online_feature_pipeline import *
+from ._online_feature_pipeline import _OnlineFeaturePipeline
 from ._online_gmm_decodable import *
 from ._online_gmm_decoding import *
 from ._online_ivector_feature import *
@@ -8,21 +9,31 @@ from ._online_ivector_feature import _OnlineIvectorExtractionInfo
 from ._online_nnet2_feature_pipeline import *
 from ._online_nnet3_decoding import *
 
-from kaldi.matrix import _postprocess_matrix
+from kaldi.matrix import SubMatrix, DoubleSubMatrix
 
 class OnlineIvectorExtractionInfo(_OnlineIvectorExtractionInfo):
-    
-    # FIXME (VM):
-    # Postprocess only takes FloatMatrix
-    # def get_global_cmvn_stats(self):
-    #     return _postprocess_matrix(self._global_cmvn_stats)
-    # def set_global_cmvn_stats(self, global_cmvn_stats):
-    #     self._global_cmvn_stats = global_cmvn_stats
+    @property
+    def lda_mat(self):
+        return SubMatrix(self._lda_mat)
 
-    # global_cmvn_stats = property(get_global_cmvn_stats, set_global_cmvn_stats)
+    @lda_mat.setter
+    def lda_mat(self, value):
+        self._lda_mat = value
 
+    @property
+    def global_cmvn_stats(self):
+        return DoubleSubMatrix(self._get_global_cmvn_stats())
 
+    @global_cmvn_stats.setter
+    def global_cmvn_stats(self, value):
+        self._global_cmvn_stats = value
 
+class OnlineFeaturePipeline(_OnlineFeaturePipeline):
+    def get_as_matrix(self):
+        return SubMatrix(self._get_as_matrix())
+
+    def new(self):
+        return OnlineFeaturePipeline(self._new())
 
 __all__ = [name for name in dir()
            if name[0] != '_'
