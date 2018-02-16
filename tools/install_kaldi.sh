e24eda82142eaa82cc29e34e14552d36679ac3cc
Dogan Can <dogancanbaz@gmail.com>
2018-02-15 20:44:48 -0800

    update kaldi script.

--- tools/install_kaldi.sh
+++ tools/install_kaldi.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# 
-# 
 # Installation script for Kaldi
-# 
+#
 set -x -e
 
 KALDI_GIT="-b pykaldi https://github.com/pykaldi/kaldi.git"
 
-KALDI_DIR="$PWD"
-if [ -n "$1" ]; then
-	KALDI_DIR="$1"
-	shift
-fi
+KALDI_DIR="$PWD/kaldi"
 
 if [ ! -d "$KALDI_DIR" ]; then
 	git clone $KALDI_GIT $KALDI_DIR
@@ -22,17 +16,16 @@ fi
 
 cd "$KALDI_DIR/tools"
 
-# Prevent kaldi from switching default python versions
+# Prevent kaldi from switching default python version
 mkdir -p "python"
 touch "python/.use_default_python"
 
 ./extras/check_dependencies.sh
 
-make -j4 
+make -j4
 
 cd ../src
 ./configure --shared
 make clean -j && make depend -j && make -j4
 
 echo "Done installing kaldi"
-touch "$KALDI_DIR/.DONE"
