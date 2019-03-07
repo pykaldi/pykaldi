e472b913db7eb4aa9e5d9e834c7d52ffaa64878c
Dogan Can <dogancanbaz@gmail.com>
2019-03-07 00:37:41 -0800

    Update models.sh

--- examples/setups/aspire/models.sh
+++ examples/setups/aspire/models.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-wget https://lowerquality.com/gentle/kaldi-models-0.03.zip
+wget --no-check-certificate https://lowerquality.com/gentle/kaldi-models-0.03.zip
 unzip kaldi-models-0.03.zip
 rm -f kaldi-models-0.03.zip
 
-wget https://lowerquality.com/gentle/aspire-hclg.tar.gz
+wget --no-check-certificate https://lowerquality.com/gentle/aspire-hclg.tar.gz
 tar -xzf aspire-hclg.tar.gz
 rm -f aspire-hclg.tar.gz
 
