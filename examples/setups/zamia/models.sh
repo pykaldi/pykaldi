3e287ce6312e09cd71b5dc635be6b0b228abd737
Dogan Can <dogancanbaz@gmail.com>
2019-03-07 00:40:41 -0800

    Update models.sh

--- examples/setups/zamia/models.sh
+++ examples/setups/zamia/models.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-wget http://goofy.zamia.org/zamia-speech/asr-models/kaldi-generic-en-tdnn_f-r20180901.tar.xz
-tar -xJf kaldi-generic-en-tdnn_f-r20180901.tar.xz
-rm -f kaldi-generic-en-tdnn_f-r20180901.tar.xz
+wget --no-check-certificate http://goofy.zamia.org/zamia-speech/asr-models/kaldi-generic-en-tdnn_f-r20190227.tar.xz
+tar -xJf kaldi-generic-en-tdnn_f-r20190227.tar.xz
+rm -f kaldi-generic-en-tdnn_f-r20190227.tar.xz
 
-mv kaldi-generic-en-tdnn_f-r20180901/README.md README-ZAMIA.md
-mv kaldi-generic-en-tdnn_f-r20180901/* .
-rm -rf kaldi-generic-en-tdnn_f-r20180901
+mv kaldi-generic-en-tdnn_f-r20190227/README.md README-ZAMIA.md
+mv kaldi-generic-en-tdnn_f-r20190227/* .
+rm -rf kaldi-generic-en-tdnn_f-r20190227
 
 mkdir -p exp/nnet3_chain
 mv model exp/nnet3_chain/tdnn_f
