8f26989a07c3f1fad8510c3c73dc4095805d4f73
muratam39 <muratam39@bbwcfs.local127.0.0.1>
2021-04-07 11:32:59 +0900

    fix the version of zamia in models.sh

--- examples/setups/zamia/models.sh
+++ examples/setups/zamia/models.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 wget --no-check-certificate https://goofy.zamia.org/zamia-speech/asr-models/kaldi-generic-en-tdnn_f-r20190609.tar.xz
-tar -xJf kaldi-generic-en-tdnn_f-r20190227.tar.xz
-rm -f kaldi-generic-en-tdnn_f-r20190227.tar.xz
+tar -xJf kaldi-generic-en-tdnn_f-r20190609.tar.xz
+rm -f kaldi-generic-en-tdnn_f-r20190609.tar.xz
 
-mv kaldi-generic-en-tdnn_f-r20190227/README.md README-ZAMIA.md
-mv kaldi-generic-en-tdnn_f-r20190227/* .
-rm -rf kaldi-generic-en-tdnn_f-r20190227
+mv kaldi-generic-en-tdnn_f-r20190609/README.md README-ZAMIA.md
+mv kaldi-generic-en-tdnn_f-r20190609/* .
+rm -rf kaldi-generic-en-tdnn_f-r20190609
 
 mkdir -p exp/nnet3_chain
 mv model exp/nnet3_chain/tdnn_f
@@ -14,3 +14,4 @@ mv extractor exp/nnet3_chain/.
 mv ivectors_test_hires exp/nnet3_chain/.
 ln -sr exp/nnet3_chain/ivectors_test_hires/conf/ivector_extractor.conf conf/.
 ln -sr ../aspire/data/test data/.
+
